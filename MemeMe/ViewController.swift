72693194adfbf4d635a0077114afeb955aeecd7f
zhehui <zhehui.zhou@outlook.com>
2017-07-05 00:55:51 -0400

    add delegate for image picker

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // MARK: Outlets
+    // MARK: Fields
+    
+    let imagePicker = UIImagePickerController()
 
+    // MARK: Outlets
+    
     @IBOutlet weak var imagePickerView: UIImageView!
     
     // MARK: UIViewController Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
     }
 
     // MARK: Actions
     
     @IBAction func pickAnImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        self.present(pickerController, animated: true, completion: nil)
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.contentMode = .scaleAspectFit
+            imagePickerView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
